941f3f032838a649997b1e4b1008a954ba2cba54
Sumit Vajarinkar <vajarinkarss@gmail.com>
2021-06-08 13:23:44 +0545

    Update input.java

--- input.java
+++ input.java
@@ -9,16 +9,21 @@ public class input {
         System.out.println("Enter you age\n");
         
         // int
-        int age =sc.nextInt();
+        int age =sc.nextInt();        
         System.out.println(age);
         
         // float
         float age1 =sc.nextFloat();
         System.out.println(age1);
         
-        // string21
-        Scanner s1 = new Scanner(System.in);
-        String name=s1.nextLine();
+        // string
+        String name=sc.nextLine();
         System.out.println(name);
+
+        // Char
+        char grade = sc.next().charAt(0);
+        System.out.println(grade);
+
+        sc.close();
     }
 }
